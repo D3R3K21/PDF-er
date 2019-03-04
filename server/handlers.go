f06151fa640d1b4453a7a921e047c0d30aceb3c7
Derek Rose <derek.s.rose21@gmail.com>
2019-03-03 17:46:26 -0700

    added logs

--- server/handlers.go
+++ server/handlers.go
@@ -2,14 +2,16 @@ package server
 
 import (
 	"bytes"
+	"fmt"
 	"integrate-pdf-service/models"
 	service "integrate-pdf-service/pdf-service"
 	"io"
 	"net/http"
 )
 
-//SignatureFunc - localhost/signature?name=derek&email=drose@integrate.com&ip=192.168.1.1&company=integrate inc
+//SignatureFunc - http://release-facebook.integrate.team:35430/signature?name=derek&email=drose@integrate.com&ip=192.168.1.1&company=integrate inc
 func SignatureFunc(response http.ResponseWriter, r *http.Request) {
+
 	response.Header().Set("Content-Disposition", "attachment; filename=sigFile.pdf")
 	response.Header().Set("Content-Type", r.Header.Get("Content-Type"))
 	requestModel := models.RetrieveSignatureRequest{
@@ -19,9 +21,16 @@ func SignatureFunc(response http.ResponseWriter, r *http.Request) {
 		IP:      r.URL.Query().Get("ip"),
 		Name:    r.URL.Query().Get("name"),
 	}
+	fmt.Println("Company :" + requestModel.Company)
+	fmt.Println("Date :" + requestModel.Date)
+	fmt.Println("Email :" + requestModel.Email)
+	fmt.Println("IP :" + requestModel.IP)
+	fmt.Println("Name :" + requestModel.Name)
 	pdf := service.GenerateSignature(requestModel)
+
 	var buf bytes.Buffer
 	pdf.Output(&buf)
+	fmt.Printf("File Size : %d", buf.Len())
 	var reader io.Reader
 	bts := buf.Bytes()
 	reader = bytes.NewReader(bts)
